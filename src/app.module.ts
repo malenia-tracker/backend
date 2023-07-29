036643634b5294f7b1787346572675faa925d021
M.Salih Gumus <gumusmuhammedsalih@gmail.com>
2023-07-29 19:36:14 +0300

    [Salih] add auth controller,module and service

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [ConfigModule.forRoot(), AuthModule],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
+export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app/app.module';
+import { AppModule } from './app.module';
 
 const PORT = process.env.PORT || 3000;
 
